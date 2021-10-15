bd69051ee30e94f327fbd43b6bad849efc3f2930
Dieuer <58150918+Dieuer@users.noreply.github.com>
2021-10-15 20:54:15 +0200

    Adding to gitignore

--- OOPEksamen/EksamenOpgaveOOP/Program.cs
+++ OOPEksamen/EksamenOpgaveOOP/Program.cs
@@ -9,7 +9,6 @@ namespace EksamenOpgaveOOP
     {
         static void Main(string[] args)
         {
-            //Stregsystem stregsystem = new Stregsystem();
             IStregsystem stregsystem = new Stregsystem();
             IStregsystemUI ui = new StregsystemCLI(stregsystem);
             StregsystemController sc = new StregsystemController(ui, stregsystem);
